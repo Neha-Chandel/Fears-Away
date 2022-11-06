a79dde2edf1d7429e7070989398b495272c7b99f
Neha-Chandel <91161212+Neha-Chandel@users.noreply.github.com>
2022-11-06 10:12:12 -0500

    'Added_authentication'

--- Assignment_2_Twitter_Scraping/fearsaway.py
+++ Assignment_2_Twitter_Scraping/fearsaway.py
@@ -16,3 +16,17 @@ api_key_secret = config['twitter']['api_key_secret']
 acess_token = config['twitter']['access_token']
 acess_token_secret = config['twitter']['access_token_secret']
 
+#Authentication 
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(acess_token, acess_token_secret)
+
+api = tweepy.API(auth)
+
+
+now = datetime.date.today()
+date_since = datetime.datetime.strptime(time.strftime("%Y-%m-%d"), "%Y-%m-%d")
+print (date_since)
+num_tweets=50
+print ("num_tweets ", num_tweets)
+
+new_search = "#crime -filter:retweets"
