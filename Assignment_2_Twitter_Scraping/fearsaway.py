5f765532c0b7ad35a8e391867d84a801c912d0f7
Neha-Chandel <91161212+Neha-Chandel@users.noreply.github.com>
2022-11-12 23:20:24 -0500

    'Final_Changes'

--- Assignment_2_Twitter_Scraping/fearsaway.py
+++ Assignment_2_Twitter_Scraping/fearsaway.py
@@ -47,6 +47,29 @@ tdata = []
 ucolumns = [ 'twitter_handle', 'user_name', 'user_location', 'user_type', 'total_tweets', 'follower_count', 'following_count', 'created_at']
 udata=[]
 
+#Appending data
+
+for tweet in tweets:
+    tdata.append([tweet.id,tweet.user.name,  tweet.user.description, tweet.created_at, tweet.entities['hashtags'], tweet.retweet_count]) 
+    udata.append([tweet.user.name, tweet.user.screen_name, tweet.user.location, tweet.user.verified, tweet.user.statuses_count, tweet.user.followers_count, tweet.user.friends_count, tweet.user.created_at])
+    cnt=cnt+1
+
+#Using the dataframes
+#I have created 2 dataframes as I am using the data from 2 tables and inserting the data from
+#2 different cvs files
+
+df1 = pd.DataFrame(udata, columns=ucolumns)
+df2 = pd.DataFrame(tdata,columns=tcolumns)
+
+print(df1)
+print(df2)
+
+#Converting the dataframes to cvs file
+df1.to_csv('tweets1.csv', index=False)
+df2.to_csv('tweets2.csv', index=False)
+
+	
+
 
 
 	
